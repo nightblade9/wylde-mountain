2b6983955ad65709a359ff7b9f50f94a35ff12b5
nightblade9 <nightbladecodes@gmail.com>
2020-08-05 16:11:08 -0400

    Changes to build/launch in VScode Remove HSTS when developing

--- source/HydraPeak.Web/Startup.cs
+++ source/HydraPeak.Web/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -42,9 +41,9 @@ namespace HydraPeak.Web
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
