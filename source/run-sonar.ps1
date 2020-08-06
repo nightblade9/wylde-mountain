a70e91ed745d6fe4614af2839c9f6e5295a52d15
nightblade9 <nightbladecodes@gmail.com>
2020-08-06 16:59:51 -0400

    Unit tests code coverage

--- source/run-sonar.ps1
+++ source/run-sonar.ps1
@@ -3,6 +3,6 @@
 # dotnet tool install --global dotnet-sonarscanner --version 4.8.0
 # coverlet: dotnet tool install --global coverlet.console
 
-dotnet sonarscanner begin /key:"HydraPeak" /d:sonar.cs.opencover.reportsPaths=HydraPeak.Web.Tests/coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs"
-dotnet build #test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+dotnet sonarscanner begin /key:"HydraPeak" /d:sonar.cs.opencover.reportsPaths=HydraPeak.Web.Tests/coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs","**/*.test*.tsx"
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 dotnet sonarscanner end
