abd7ea19a22edc836d2e7ea7e632bca9fd53d492
nightblade9 <nightbladecodes@gmail.com>
2020-08-06 16:44:08 -0400

    Controller tests for web services

---
+++ source/HydraPeak.Web.Tests/Controllers/RegisterControllerTests.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Linq.Expressions;
+using HydraPeak.Web.Controllers;
+using HydraPeak.Web.DataAccess.Repositories;
+using HydraPeak.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using Moq;
+using NUnit.Framework;
+using static HydraPeak.Web.Controllers.RegisterController;
+
+namespace HydraPeak.Web.Tests.Controllers
+{
+    [TestFixture]
+    public class RegisterControllerTests
+    {
+
+        [Test]
+        public void RegisterSucceeds()
+        {
+            // Arrange
+            const string expectedEmail = "test@test.com";
+            const string expectedPassword = "password";
+
+            User expectedUser = null;
+            var request = new RegistrationRequest() { EmailAddress = expectedEmail, Password = expectedPassword };
+
+            var repository = new Mock<IGenericRepository>();
+            repository.Setup(u => u.Insert(It.IsAny<User>())).Callback<User>((user) =>
+            {
+                Assert.That(user, Is.Not.Null);
+                Assert.That(user.EmailAddress, Is.EqualTo(expectedEmail));
+                repository.Setup(u => u.SingleOrDefault(It.IsAny<Expression<Func<User, bool>>>())).Returns(user);
+                user.Id = ObjectId.GenerateNewId();
+                expectedUser = user;
+            });
+
+            repository.Setup(a => a.Insert(It.IsAny<Auth>())).Callback<Auth>((auth) =>
+            {
+                Assert.That(auth, Is.Not.Null);
+                Assert.That(auth.HashedPassword, Is.EqualTo(expectedPassword));
+            });
+
+            var controller = new RegisterController(new Mock<ILogger<RegisterController>>().Object, repository.Object);
+
+            // Act
+            var response = controller.Register(request).Result;
+
+            // Assert: response is OK and records were inserted (callbacks invoked)
+            Assert.That(response, Is.TypeOf(typeof(OkObjectResult)));
+            var obj = ((OkObjectResult)response).Value;
+            Assert.That(obj, Is.EqualTo(expectedUser));
+            repository.VerifyAll();
+        }
+
+        [Test]
+        public void RegisterThrowsIfUserNameAlreadyExistsInDb()
+        {
+            // Arrange
+            const string expectedEmail = "test@test.com";
+            const string expectedPassword = "password";
+
+            var request = new RegistrationRequest() { EmailAddress = expectedEmail, Password = expectedPassword };
+
+            var existingUser = new User() { EmailAddress = expectedEmail, Id = new MongoDB.Bson.ObjectId() };
+            var repository = new Mock<IGenericRepository>();
+            repository.Setup(u => u.SingleOrDefault(It.IsAny<Expression<Func<User, bool>>>())).Returns(existingUser);
+
+            var controller = new RegisterController(new Mock<ILogger<RegisterController>>().Object, repository.Object);
+
+            // Act
+            var response = controller.Register(request).Result;
+
+            // Assert
+            Assert.That(response, Is.TypeOf(typeof(BadRequestObjectResult)));
+            var obj = ((BadRequestObjectResult)response).Value;
+            Assert.That(obj, Is.TypeOf(typeof(ArgumentException)));
+        }
+    }
+}
