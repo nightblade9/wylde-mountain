c1f4808daa232938e92aa637b63ad597a5d11b21
nightblade9 <nightbladecodes@gmail.com>
2020-08-26 16:05:30 -0400

    Localization for registration page Consolidate messages, user name only (no first/last name)

--- source/WyldeMountain.Web/ClientApp/src/components/Register.tsx
+++ source/WyldeMountain.Web/ClientApp/src/components/Register.tsx
@@ -1,7 +1,9 @@
-import React, { ChangeEvent, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { globalSettings } from '../App';
 import { Container, CssBaseline, Avatar, Typography, TextField, FormControlLabel, Checkbox, Button, Grid, Box, Link } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { LockOutlined as LockOutlinedIcon } from '@material-ui/icons'
+import React, { ChangeEvent, useState } from 'react';
+import LocalizedStrings from 'react-localization';
 import { Link as RouterLink, useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
@@ -30,9 +32,16 @@ const Register = (props:any) => {
   // use state hooks
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [lastMessage, setMessage] = useState<string>("");
 
   // for routing
   const history = useHistory();
+
+  const languageStrings = new LocalizedStrings({
+    "en": require('~/../../resources/components/Register-en.json')//,
+    //"ar": require('~/../../resources/components/Login-ar.json')
+  });
+  languageStrings.setLanguage(globalSettings.language);
 
   const onSumbit = (event: ChangeEvent<HTMLFormElement>) => {
     let state = {emailAddress: email, password: password};
@@ -53,14 +62,14 @@ const Register = (props:any) => {
           return response.json();
         }
         else {
-          throw new Error("Registration failed!");
+          throw new Error(languageStrings.registrationFailed);
         }
       })
       .then(data => {
-        console.log("Registration succeeded");
+        setMessage(languageStrings.registrationSucceeded)
         history.push("/login");
       })
-      .catch(e => console.error("Registration failed: " + e));
+      .catch(e => setMessage(languageStrings.registrationFailed));
   }
 
   return (
@@ -72,29 +81,19 @@ const Register = (props:any) => {
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
+        <div>{lastMessage}</div>
         <form className={classes.form} noValidate onSubmit={onSumbit}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
               <TextField
-                autoComplete="fname"
-                name="firstName"
+                autoComplete="username"
+                name="userName"
                 variant="outlined"
                 required
                 fullWidth
-                id="firstName"
-                label="First Name"
+                id="userName"
+                label={languageStrings.userName}
                 autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
               />
             </Grid>
             <Grid item xs={12}>
@@ -103,7 +102,7 @@ const Register = (props:any) => {
                 required
                 fullWidth
                 id="email"
-                label="Email Address"
+                label={languageStrings.emailAddress}
                 name="email"
                 autoComplete="email"
                 onChange={e => setEmail(e.target.value)}
@@ -115,17 +114,11 @@ const Register = (props:any) => {
                 required
                 fullWidth
                 name="password"
-                label="Password"
+                label={languageStrings.password}
                 type="password"
                 id="password"
                 autoComplete="current-password"
                 onChange={e => setPassword(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
               />
             </Grid>
           </Grid>
@@ -136,12 +129,12 @@ const Register = (props:any) => {
             color="primary"
             className={classes.submit}
           >
-            Sign Up
+            {languageStrings.register}
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
               <Link to="/login" variant="body2" component={RouterLink}>
-                Already have an account? Sign in
+                {languageStrings.signIn}
               </Link>
             </Grid>
           </Grid>
