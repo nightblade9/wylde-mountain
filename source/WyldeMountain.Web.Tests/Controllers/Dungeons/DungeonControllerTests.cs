f3fb0d16bad1b1f897e5b302b2a9033e2d32cca6
nightblade9 <nightbladecodes@gmail.com>
2020-08-28 13:59:45 -0400

    Positive test-case for /dungeon API call

---
+++ source/WyldeMountain.Web.Tests/Controllers/Dungeons/DungeonControllerTests.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using Moq;
+using NUnit.Framework;
+using WyldeMountain.Web.Controllers.Dungeons;
+using WyldeMountain.Web.DataAccess.Repositories;
+using WyldeMountain.Web.Models.Authentication;
+using WyldeMountain.Web.Models.Dungeons;
+
+namespace WyldeMountain.Web.Tests.Controllers.Dungeons
+{
+    [TestFixture]
+    public class DungeonControllerTests
+    {
+        [Test]
+        public void GetReturnsVisibleEventsOnly()
+        {
+            // Arrange
+            var repository = new Mock<IGenericRepository>();
+            
+            var controller = new DungeonController(new Mock<ILogger<DungeonController>>().Object, repository.Object);
+            controller.CurrentUser = new User();
+            var dungeon = new Dungeon() { CurrentFloor = new Floor(1) };
+            repository.Setup(r => r.SingleOrDefault<Dungeon>(It.IsAny<Expression<Func<Dungeon, bool>>>())).Returns(dungeon);
+            
+            // Act
+            var response = controller.Get();
+
+            // Assert
+            Assert.That(response, Is.TypeOf(typeof(ActionResult<Dungeon>)));
+            var actualDungeon = ((ObjectResult)response.Result).Value as Dungeon;
+            Assert.That(actualDungeon, Is.Not.Null);
+            foreach (var choice in actualDungeon.CurrentFloor.Events)
+            {
+                Assert.That(choice.Count, Is.EqualTo(1)); // four events but we only see the first/visible one
+            }
+        }
+
+        [Test]
+        public void GetReturnsBadRequestIfUserIsNull()
+        {
+        }
+
+        [Test]
+        public void GetReturnsBadRequestIfDungeonIsNull()
+        {
+        }
+    }
+}
