b1ca3d1b7647bb51833e40805a970b0a86b39e80
nightblade9 <nightbladecodes@gmail.com>
2020-08-28 14:43:53 -0400

    Localization for authentication header

--- source/WyldeMountain.Web/ClientApp/src/components/Authentication/Identity.tsx
+++ source/WyldeMountain.Web/ClientApp/src/components/Authentication/Identity.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import { globalSettings } from '../../App';
 import JwtBody from '../../interfaces/Jwt';
 import jwtDecode from 'jwt-decode';
+import React from 'react';
+import LocalizedStrings from 'react-localization';
 
 interface TokenProps {
   token: string;
 }
 
 export function Identity(props: TokenProps) {
+  const languageStrings = new LocalizedStrings({
+    "en": require('~/../../resources/components/Authentication/Identity-en.json'),
+    "ar": require('~/../../resources/components/Authentication/Identity-ar.json')
+  });
+  languageStrings.setLanguage(globalSettings.language);  
+  
   return (
-    <span>{props.token != null ? "Hi, " + (jwtDecode(props.token) as JwtBody).email : "Unauthenticated"}</span>
+    <span>{props.token != null ? (jwtDecode(props.token) as JwtBody).email : languageStrings.unauthenticated}</span>
   )
 }
